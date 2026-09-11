839c3b74b59a435e3847ee249e36dc4adbbe0391
Florian Trück <ftrueck@gmail.com>
2026-09-11 18:41:22 +0200

    Limit public API to app settings

--- api.js
+++ api.js
@@ -1,60 +1,6 @@
 'use strict';
 
-const { randomUUID } = require('crypto');
-
 module.exports = {
-
-  async getRemotes({ homey }) {
-    return homey.app.getRemoteDevices().map((device) => ({
-      id: device.getData().id,
-      name: device.getName(),
-      buttons: device.getButtons(),
-    }));
-  },
-
-  async addButton({ homey, params, body }) {
-    const device = homey.app.getRemote(params.deviceId);
-    const buttons = device.getButtons();
-    const [button] = homey.app.validateButtons([{ ...body, id: randomUUID() }]);
-    buttons.push(button);
-    await device.setButtons(buttons);
-    return button;
-  },
-
-  async updateButton({ homey, params, body }) {
-    const device = homey.app.getRemote(params.deviceId);
-    const buttons = device.getButtons();
-    const index = buttons.findIndex((button) => button.id === params.buttonId);
-    if (index < 0) throw new Error('Button not found');
-    const [button] = homey.app.validateButtons([{ ...buttons[index], ...body, id: params.buttonId }]);
-    buttons[index] = button;
-    await device.setButtons(buttons);
-    return button;
-  },
-
-  async deleteButton({ homey, params }) {
-    const device = homey.app.getRemote(params.deviceId);
-    const buttons = device.getButtons();
-    const filtered = buttons.filter((button) => button.id !== params.buttonId);
-    if (filtered.length === buttons.length) throw new Error('Button not found');
-    await device.setButtons(filtered);
-    return true;
-  },
-
-  async learnButton({ homey, params }) {
-    const device = homey.app.getRemote(params.deviceId);
-    const buttons = device.getButtons();
-    const index = buttons.findIndex((button) => button.id === params.buttonId);
-    if (index < 0) throw new Error('Button not found');
-    buttons[index] = { ...buttons[index], code: await homey.app.mqtt.learn() };
-    await device.setButtons(buttons);
-    return buttons[index];
-  },
-
-  async sendButton({ homey, params }) {
-    await homey.app.getRemote(params.deviceId).sendButton(params.buttonId);
-    return true;
-  },
 
   async getMqtt({ homey }) {
     const config = homey.app.mqtt.getConfig();
